ca1e1a5fbc6f424a5173bf89ba6e39e03765b9fb
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-05-05 14:22:10 +0400

    Added style for image rendering

--- src/components/CardList/CardsList.jsx
+++ src/components/CardList/CardsList.jsx
@@ -1,13 +1,15 @@
+import styles from './CardList.module.css';
+
 const CardsList = ({ data }) => {
   return (
     <>
-      <ul>
+      <ul className={styles['cards-grid']}>
         {data.map((card) => (
-          <li key={card._id}>
-            <div>
+          <li key={card._id} className={styles.card}>
+            <div className={styles['card-image']}>
               <img src={card.imageUrl} alt={card.name} />
             </div>
-            <h2>{card.name}</h2>
+            <h2 className={styles['card-title']}>{card.name}</h2>
           </li>
         ))}
       </ul>
