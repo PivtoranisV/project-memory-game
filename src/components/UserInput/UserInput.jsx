e907f2364d81556f77bde93517c5b7cc97b47d8f
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-05-07 13:07:45 +0400

    Added useRef to read userInput value

--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,23 +1,43 @@
+import { useRef } from 'react';
+
 import UserInputWrapper from './UserInputWrapper';
 
 import styles from './UserInput.module.css';
 
 const UserInput = () => {
+  const topicRef = useRef();
+  const levelRef = useRef();
+  const pageRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredTopic = topicRef.current.value;
+    const enteredLevel = levelRef.current.value;
+    const enteredPage = pageRef.current.value;
+
+    console.log(enteredLevel, enteredPage, enteredTopic);
+  };
+
   return (
     <UserInputWrapper>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={styles['control-group']}>
           <div className={styles['form-control']}>
             <label htmlFor="topic">Game Topic</label>
-            <input type="text" id="topic" />
+            <input
+              type="text"
+              id="topic"
+              placeholder="Cars, Nature, Space, Lviv etc."
+              ref={topicRef}
+            />
           </div>
           <div className={styles['form-control']}>
             <label htmlFor="level">Game Level (7-30)</label>
-            <input type="number" id="level" min="7" max="30" />
+            <input type="number" id="level" min="7" max="30" ref={levelRef} />
           </div>
           <div className={styles['form-control']}>
             <label htmlFor="level">Lucky Number (1-100)</label>
-            <input type="number" id="level" min="1" max="100" />
+            <input type="number" id="level" min="1" max="100" ref={pageRef} />
           </div>
         </div>
         <div className={styles['form-actions']}>
